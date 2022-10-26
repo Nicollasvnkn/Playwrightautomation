350c7f85dac8d41825d68295f6964e4581420f53
Nicollasvnkn <71653567+Nicollasvnkn@users.noreply.github.com>
2022-10-25 22:29:45 -0300

    commit 1

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,35 @@
+const {test, expect} = require('@playwright/test');
+//const { expect } = require('.../playwright.config');
+
+
+test.only('Browser Context Error login following the correct login in the same test', async ({browser}) =>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+
+    await userName.type("rahulshetty");
+    await page.locator("[type='password']").type('learning');
+    await signIn.click();
+    console.log (await page.locator("[style*=block]").textContent());
+    await expect(page.locator("[style*=block]")).toContainText('Incorrect');
+
+    await userName.type("");
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+
+    //To access the fist element it can work eith those two lines of code
+    console.log(await page.locator(".card-body a").first().textContent());
+    console.log(await page.locator(".card-body a").nth(1).textContent());
+});
+
+test('Page Playwright test', async ({page}) =>
+{
+    await page.goto("https://google.com");
+    //get title - assertion
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+});
